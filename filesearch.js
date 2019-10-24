2b67ec4ec4e992d3828565ccc20071c587609079
Kyler Daybell <kyler.daybell@ad.snow.edu>
2019-10-24 17:27:53 -0600

    we can save files so yeah

--- filesearch.js
+++ filesearch.js
@@ -3,20 +3,9 @@ const {dialog} = require('electron').remote;
 let content = "Some file that is fun";
 
 
-function startSave(filename){
-    dialog.showSaveDialog((filename) => {
-        console.log("in savefile")
-        if (filename === undefined){
-            console.log("you didn't save the file");
-            return;
-        }
-    
-        fs.writeFile(filename, content, (err) => {
-            if(err){
-                alert("an error ocurred creating the file");
-            }
-            alert("The file has ben saved successfully");
-        });
+function saveFile(filepath,filename,contents){
+    fs.writeFile("C:/Users/Kyler.Daybell/Desktop/kyler.txt","kyler was here",()=>{
+        return;
     });
 }
 
