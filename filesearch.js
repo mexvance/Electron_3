e1128d337e02f5649c837b6482539c709e0c6991
Kyler Daybell <kyler.daybell@ad.snow.edu>
2019-10-24 17:13:54 -0600

    we are able to open the file save dialog

--- filesearch.js
+++ filesearch.js
@@ -1,21 +1,22 @@
-var app = require('electron').remote;
-var dialog = app.dialog;
 var fs = require('fs');
-
 const {dialog} = require('electron').remote;
-
 let content = "Some file that is fun";
 
-dialog.showSaveDialog((filename) => {
-    if (filename === undefined){
-        console.log("you didn't save the file");
-        return;
-    }
 
-    fs.writeFile(filename, content, (err) => {
-        if(err){
-            alert("an error ocurred creating the file");
+function startSave(filename){
+    dialog.showSaveDialog((filename) => {
+        console.log("in savefile")
+        if (filename === undefined){
+            console.log("you didn't save the file");
+            return;
         }
-        alert("The file has ben saved successfully");
+    
+        fs.writeFile(filename, content, (err) => {
+            if(err){
+                alert("an error ocurred creating the file");
+            }
+            alert("The file has ben saved successfully");
+        });
     });
-});
+}
+
