75efda3eac1a953fe3d980f39519703d444f2adb
Brandon Isbell <brandonisbell1997@gmail.com>
2019-10-25 11:58:20 -0600

    a lot of the features are here

--- filesearch.js
+++ filesearch.js
@@ -1,34 +1,35 @@
-var fs = require('fs');
-const {dialog} = require('electron').remote;
-let content = "Some file that is fun";
+var fs = require("fs");
+const { dialog } = require("electron").remote;
 
-
-function saveFile(filepath,filename,contents){
-    dialog.showSaveDialog(null, {properties:["OpenDirectory"]})
-    .then(result=>{
-        console.log(result);
-        filename = result.filePath;
-        console.log(filename);
-        fs.writeFile(filename,"brandon was here",()=>{
-            return;
-        });
-    })
+function saveFile(filepath, filename, contents) {
+  dialog
+    .showSaveDialog(null, { properties: ["OpenDirectory"] })
+    .then(result => {
+      console.log(result);
+      filename = result.filePath;
+      console.log(filename);
+      contents = document.getElementById("fileinfoedit").innerText;
+      fs.writeFile(filename, contents, () => {
+        return;
+      });
+    });
 }
 
-function readFile(){
-    console.log("In ReadFile");
-    let filepath;
-    dialog.showOpenDialog(null, {properties:["OpenDirectory"]})
-    .then(result=>{
-        filepath = result.filePaths[0];
-        console.log(filepath);
-        fs.readFile(filepath, 'utf-8', (err, data) => {
-            if(err){
-                alert("An error ocurred reading the file :" + err.message);
-                return;
-            }
-            console.log("The file content is : " + data);
-            document.getElementById("fileinfoedit").innerHTML = data;
-        });
+function readFile() {
+  console.log("In ReadFile");
+  let filepath;
+  dialog
+    .showOpenDialog(null, { properties: ["OpenDirectory"] })
+    .then(result => {
+      filepath = result.filePaths[0];
+      console.log(filepath);
+      fs.readFile(filepath, "utf-8", (err, data) => {
+        if (err) {
+          alert("An error ocurred reading the file :" + err.message);
+          return;
+        }
+        console.log("The file content is : " + data);
+        document.getElementById("fileinfoedit").innerText = data;
+      });
     });
 }
